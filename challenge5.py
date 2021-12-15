517136c21da298f0606392b9b4721bb813e11207
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-15 15:09:42 +0100

    auto push

--- challenge5.py
+++ challenge5.py
@@ -1,3 +1,15 @@
+import re
 with open('Invictus.txt') as f:
     lines = f.readlines()
-    print(lines)
+print(lines)
+# print(b'\U000e0077'.decode("utf-8", "strict"))
+unicodes=[]
+# print(m.group(0))
+for l in lines:
+    m = re.search('U.{8}', l)
+    # print(u'\u323')
+    unicodes.append(l.replace('Out',''))
+print(unicodes)
+
+
+
