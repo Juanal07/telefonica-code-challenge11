d15a2bc32150a23858f67fee376721052980dc2d
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-18 17:55:19 +0100

    auto push

--- challenge9.py
+++ challenge9.py
@@ -1,7 +1,7 @@
 def challenge9(input):
     # f = open("sampleOutput.txt", "w")
     # f = open("testOutput.txt", "w")
-    # f = open("submitOutput.txt", "w")
+    f = open("submitOutput.txt", "w")
     cases = int(input.readline())
     n_sprites = int(input.readline())
     j=1
@@ -22,47 +22,35 @@ def challenge9(input):
     # print(sprites)
     i=1
     while i<=cases:
-        active=[0]*n_sprites
-        print('Caso: ',i)
-        sprites_aux=sprites.copy()
+        print('Caso:',i)
         movs=input.readline()
+        lista_sprites=[]
         num_colisiones=0
+        colision=False
         for _ in range(int(movs)):
+            colision=False
             mov=input.readline()
             mov=mov.split(' ')
             new_sprite=[]
-            for e in sprites_aux[int(mov[0])]:
+            for e in sprites[int(mov[0])]:
                 tupla=(e[0]+int(mov[1]),e[1]+int(mov[2]))
                 new_sprite.append(tupla)
-            sprites_aux[int(mov[0])] = new_sprite.copy()
-            # print(sprites_aux[int(mov[0])])
-            # print(active)
-            active[int(mov[0])]=1
-            print(active)
-            # print('sprite modificado',sprites_aux)
-            colision=False
-            for posicion_activa in range(len(active)):
-                sprite_activo=int(mov[0])
-                if active[posicion_activa]==1:
-                    if sprite_activo!=posicion_activa:
-                        for l in sprites_aux[posicion_activa]:
-                            colision = l in sprites_aux[int(mov[0])]
-                            if colision==True:
-                                num_colisiones+=1
-                                break
-                if colision==True:
-                    break
-        # print('Colisiones',num_colisiones)
+            for l in lista_sprites:
+                for n in new_sprite:
+                    colision = n in l
+                    if colision==True:
+                        num_colisiones+=1
+                        break
+            lista_sprites.append(new_sprite)
+        print('Colisiones:',num_colisiones)
+        f.write('Case #{}: {}'.format(i,num_colisiones))
+        f.write('\n')
         i+=1
+    f.close()
 
-input = open('sampleInput.txt', 'r')
+# input = open('sampleInput.txt', 'r')
 # input = open('testInput.txt', 'r')
-# input = open('submitInput.txt', 'r')
+input = open('submitInput.txt', 'r')
 challenge9(input)
 input.close()
 
-    # # f = open("submitOutput.txt", "w")
-    #             f.write('{},'.format(city))
-    #     f.write('\n')
-    #     i+=1
-    # f.close()
