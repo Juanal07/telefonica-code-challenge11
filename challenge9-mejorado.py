1e2dabd258994f5beabe649016dd458a0a7039a1
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-19 01:26:24 +0100

    auto push

---
+++ challenge9-mejorado.py
@@ -0,0 +1,72 @@
+def challenge9(input):
+    # f = open("sampleOutput.txt", "w")
+    # f = open("testOutput.txt", "w")
+    # f = open("submitOutput.txt", "w")
+    cases = int(input.readline())
+    n_sprites = int(input.readline())
+    j=1
+    sprites=[]
+    dimensiones=[]
+    while j<=n_sprites:
+        size = input.readline()
+        size = size.split(' ')
+        x=int(size[0])
+        y=int(size[1])
+        dimen=(int(x),int(y))
+        dimensiones.append(dimen)
+        sprite=[]
+        for k in range(y):
+            row=input.readline()
+            for d in range(x):
+                if row[d]=='1':
+                    sprite.append((d,k))
+        sprites.append(sprite)
+        j+=1
+    # print(dimensiones)
+    # print(sprites)
+    saltados=0
+    i=1
+    while i<=cases:
+        print('Caso:',i)
+        movs=input.readline()
+        lista_sprites=[]
+        num_colisiones=0
+        colision=False
+        checkeos=0
+        for _ in range(int(movs)):
+            colision=False
+            mov=input.readline()
+            mov=mov.split(' ')
+            coord_x=int(mov[1])
+            coord_y=int(mov[2])
+            new_sprite=[]
+            for e in sprites[int(mov[0])]:
+                tupla=(e[0]+coord_x,e[1]+coord_y)
+                new_sprite.append(tupla)
+            val=[coord_x,coord_x+dimensiones[int(mov[0])][0],coord_y,coord_y+dimensiones[int(mov[0])][1]]
+            # print(val)
+            for l in lista_sprites:
+                checkeos+=1
+                for n in new_sprite:
+                    if l[1][0]>val[1] or l[1][1]<val[0] or l[1][2]>val[3] or l[1][3]<val[2]:
+                        saltados+=1
+                        break
+                    colision = n in l[0]
+                    if colision==True:
+                        num_colisiones+=1
+                        break
+            lista_sprites.append([new_sprite,val])
+        print('Colisiones:',num_colisiones)
+        print('saltados',saltados)
+        print('checkeos',checkeos)
+        # f.write('Case #{}: {}'.format(i,num_colisiones))
+        # f.write('\n')
+        i+=1
+    # f.close()
+
+# input = open('sampleInput.txt', 'r')
+input = open('testInput.txt', 'r')
+# input = open('submitInput.txt', 'r')
+challenge9(input)
+input.close()
+
--- challenge9.py
+++ challenge9.py
@@ -1,55 +1,82 @@
 def challenge9(input):
     # f = open("sampleOutput.txt", "w")
-    # f = open("testOutput.txt", "w")
+    f = open("testOutput.txt", "w")
     # f = open("submitOutput.txt", "w")
     cases = int(input.readline())
     n_sprites = int(input.readline())
     j=1
     sprites=[]
+    dimensiones=[]
     while j<=n_sprites:
         size = input.readline()
         size = size.split(' ')
         x=int(size[0])
         y=int(size[1])
+        dimen=(int(x),int(y))
+        dimensiones.append(dimen)
         sprite=[]
-        for k in range(y):
-            row=input.readline()
-            for d in range(x):
-                if row[d]=='1':
-                    sprite.append((d,k))
+        for _ in range(y):
+            row=input.readline().replace('\n','')
+            fila=[]
+            for r in row:
+                fila.append(int(r))
+            sprite.append(fila)
         sprites.append(sprite)
         j+=1
-    print(sprites)
+    saltados=0
     i=1
     while i<=cases:
         print('Caso:',i)
         movs=input.readline()
-        lista_sprites=[]
         num_colisiones=0
-        colision=False
+        checkeos=0
+        spawns=[]
         for _ in range(int(movs)):
-            colision=False
             mov=input.readline()
             mov=mov.split(' ')
-            new_sprite=[]
-            for e in sprites[int(mov[0])]:
-                tupla=(e[0]+int(mov[1]),e[1]+int(mov[2]))
-                new_sprite.append(tupla)
-            for l in lista_sprites:
-                for n in new_sprite:
-                    colision = n in l
-                    if colision==True:
-                        num_colisiones+=1
-                        break
-            lista_sprites.append(new_sprite)
+            index=int(mov[0])
+            coord_x=int(mov[1])
+            coord_y=int(mov[2])
+            spawn=[index,(coord_x,coord_y),(coord_x+dimensiones[index][0],coord_y+dimensiones[index][1])]
+            for s in spawns:
+                intereseccion=[(max(s[1][0],spawn[1][0]),max(s[1][1],spawn[1][1])),(min(s[2][0],spawn[2][0]),min(s[2][1],spawn[2][1]))]
+                checkeos+=1
+                if intereseccion[0][0] > intereseccion[1][0] or intereseccion[0][1] > intereseccion[1][1]:
+                    saltados+=1
+                    pass
+                else:
+                    puntos_s=[(intereseccion[0][0]-s[1][0],intereseccion[0][1]-s[1][1]),(intereseccion[1][0]-s[1][0],intereseccion[1][1]-s[1][1])]
+                    puntos_spawn=[(intereseccion[0][0]-spawn[1][0],intereseccion[0][1]-spawn[1][1]),(intereseccion[1][0]-spawn[1][0],intereseccion[1][1]-spawn[1][1])]
+                    width=puntos_spawn[1][0]-puntos_spawn[0][0]
+                    heigh=puntos_spawn[1][1]-puntos_spawn[0][1]
+                    numero_s=s[0]
+                    numero_spawn=spawn[0]
+                    sprite_s=sprites[numero_s]
+                    sprite_spawn=sprites[numero_spawn]
+                    colision=False
+                    for h in range(heigh):
+                        for w in range(width):
+                            eje_x_s=puntos_s[0][0]+w
+                            eje_y_s=puntos_s[0][1]+h
+                            eje_x_spawn=puntos_spawn[0][0]+w
+                            eje_y_spawn=puntos_spawn[0][1]+h
+                            valor_s=sprite_s[eje_y_s][eje_x_s]
+                            valor_spawn=sprite_spawn[eje_y_spawn][eje_x_spawn]
+                            if valor_s==1 and valor_spawn==1:
+                                colision=True
+                                num_colisiones+=1
+                                break
+                        if colision==True:
+                            break
+            spawns.append(spawn)
         print('Colisiones:',num_colisiones)
-        # f.write('Case #{}: {}'.format(i,num_colisiones))
-        # f.write('\n')
+        f.write('Case #{}: {}'.format(i,num_colisiones))
+        f.write('\n')
         i+=1
-    # f.close()
+    f.close()
 
-input = open('sampleInput.txt', 'r')
-# input = open('testInput.txt', 'r')
+# input = open('sampleInput.txt', 'r')
+input = open('testInput.txt', 'r')
 # input = open('submitInput.txt', 'r')
 challenge9(input)
 input.close()
