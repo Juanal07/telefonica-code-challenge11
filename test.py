e9c5d4545abc8c47dcfb8295f06c7a8889adf93e
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-12-19 14:36:04 +0100

    auto push

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-from scapy.all import *
-
-# rdpcap comes from scapy and loads in our pcap file
-packets = rdpcap('icmps.pcap')
-# print(packets)
-# Let's iterate through every packet
-for packet in packets:
-    print(packets)
-    # We're only interested packets with a DNS Round Robin layer
-    if packet.haslayer(DNSRR):
-        # If the an(swer) is a DNSRR, print the name it replied with.
-        if isinstance(packet.an, DNSRR):
-            print(packet.an.rrname)
+import pcapkit
+plist = pcapkit.extract(fin='icmps.pcap', fout='out.plist', format='plist', store=False)
+json = pcapkit.extract(fin='icmps.pcap', fout='out.json', format='json', extension=False)
